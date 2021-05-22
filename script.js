bedce838fcd10117edabdf7be20c84e25eaa1d26
Ar1f007 <muhammadarif9416@yahoo.com>
2021-05-23 03:13:19 +0600

    refactored js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector(".container");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menu = document.querySelector(".menu");
 const menuLink = document.querySelectorAll(".menu-link");
 
 window.addEventListener("load", () => {
@@ -11,11 +10,6 @@ window.addEventListener("load", () => {
 
 hamburgerMenu.addEventListener("click", () => {
   container.classList.toggle("change");
-});
-
-menu.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".menu-link");
-  if (!clicked) return;
   menuLink.forEach((link) =>
     link.addEventListener("click", () => container.classList.remove("change"))
   );
