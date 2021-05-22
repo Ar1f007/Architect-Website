465e48f7e42354a6c4924ede2dd72391a9e21cc8
Ar1f007 <muhammadarif9416@yahoo.com>
2021-05-23 02:56:09 +0600

    navbar issue fixed

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const container = document.querySelector(".container");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".menu");
+const menuLink = document.querySelectorAll(".menu-link");
+
 window.addEventListener("load", () => {
   setTimeout(() => {
     document.querySelector("body").classList.add("display");
   }, 3000);
 });
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-  document.querySelector(".container").classList.toggle("change");
+hamburgerMenu.addEventListener("click", () => {
+  container.classList.toggle("change");
+});
+
+menu.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".menu-link");
+  if (!clicked) return;
+  menuLink.forEach((link) =>
+    link.addEventListener("click", () => container.classList.remove("change"))
+  );
 });
